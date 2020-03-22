fb724b709c47e9a3e95908e2a239161338045c56
hubschne <larissahubschneider@Larissas-MBP.local>
2020-03-22 11:27:18 +0100

    Updated run.py, api-key removed

--- run.py
+++ run.py
@@ -8,8 +8,14 @@ json1_file = open("user_data.json")
 json1_str = json1_file.read()
 json1_data = json.loads(json1_str)
 
+
 # Larissas api_key, feel free to change
-api_key = "AIzaSyD7MB-Afk1X3Buu0brPpYO3SK_2DILXtYQ"
+json2_file = open("api_key.json")
+json2_str = json2_file.read()
+json2_data = json.loads(json2_str)
+api_key = json2_data[0]["api_key"]
+
+# print(api_key)
 
 # types of locations that should be considered.
 # others are 'supermarket', 'food', check here:
@@ -28,6 +34,13 @@ results = populartimes.get(api_key, types, bound_lower, bound_upper, n_threads, 
 
 # create DataFrame to split Address in street and city
 df = pd.DataFrame(results)
+
+df.to_csv(r'df1.csv', index=False)
+
+# if ['current_popularity'] not in df.columns():
+if 'current_popularity' not in df.index:
+    df['current_popularity'] = 1000
+
 df = df.fillna(1000)
 df['street'] = df['address'].str.split(', ', n=1, expand=True)[0]
 df['city'] = df['address'].str.split(', ', n=1, expand=True)[1]
@@ -40,4 +53,4 @@ df = df[columns]
 df.to_json(r'output.json', orient='records')
 
 # check for consistency
-# df.to_csv(r'df2.csv', index=False)
+df.to_csv(r'df2.csv', index=False)
