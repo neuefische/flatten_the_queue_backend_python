e2f936da28c0963b31229fe1bb44c5c48f05c193
Christian Mootz <christian.mootz@mootz-automation.de>
2020-03-22 15:30:47 +0100

    new exception handling

--- run.py
+++ run.py
@@ -11,7 +11,6 @@ radius = 1000
 def main(argv):
     try:
        opts, args = getopt.getopt(argv,"hl:b:r:",["longitute=","latitude=","radius="])
-       print(opts,args)
     except getopt.GetoptError:
        print ('run.py -i <longitude> -o <outputfile>')
        sys.exit(2)
@@ -25,64 +24,69 @@ def main(argv):
           latitude = float(arg)
        elif opt in ("-r", "--radius"):
           radius = float(arg)
-    print ('longitude file is ', longitude)
-    print ('latitude file is ', latitude)
-    print ('radius file is ', radius)
+    #print ('longitude file is ', longitude)
+    #print ('latitude file is ', latitude)
+    #print ('radius file is ', radius)
     
-    #read json file with lat and lng input, till now no radius, radius is set by default to 1 km
-    json1_file = open("user_data.json")
-    json1_str = json1_file.read()
-    json1_data = json.loads(json1_str)
+    try:
+        #read json file with lat and lng input, till now no radius, radius is set by default to 1 km
+        json1_file = open("user_data.json")
+        json1_str = json1_file.read()
+        json1_data = json.loads(json1_str)
 
 
-    # Larissas api_key, feel free to change
-    json2_file = open("api_key.json")
-    json2_str = json2_file.read()
-    json2_data = json.loads(json2_str)
-    api_key = json2_data[0]["api_key"]
+        # Larissas api_key, feel free to change
+        json2_file = open("api_key.json")
+        json2_str = json2_file.read()
+        json2_data = json.loads(json2_str)
+        api_key = json2_data[0]["api_key"]
 
-    # print(api_key)
+        # print(api_key)
 
-    # types of locations that should be considered.
-    # others are 'supermarket', 'food', check here:
-    types = ["grocery_or_supermarket"]
+        # types of locations that should be considered.
+        # others are 'supermarket', 'food', check here:
+        types = ["grocery_or_supermarket"]
 
-    # default values for boundrys ( +/- 0.01)
-    bound_lower = (latitude-0.01, longitude-0.01)
-    bound_upper = (latitude+0.01, longitude+0.01)
-    n_threads = 20
-    # default
-    radius = 1000
-    all_places = False
+        # default values for boundrys ( +/- 0.01)
+        bound_lower = (latitude-0.01, longitude-0.01)
+        bound_upper = (latitude+0.01, longitude+0.01)
+        n_threads = 20
+        # default
+        all_places = False
 
-    results = populartimes.get(api_key, types, bound_lower, bound_upper, n_threads,  radius, all_places)
+        results = populartimes.get(api_key, types, bound_lower, bound_upper, n_threads,  radius, all_places)
 
 
-    # create DataFrame to split Address in street and city
-    df = pd.DataFrame(results)
+        # create DataFrame to split Address in street and city
+        df = pd.DataFrame(results)
 
-    df.to_csv(r'df1.csv', index=False)
+        df.to_csv(r'df1.csv', index=False)
 
-    # if ['current_popularity'] not in df.columns():
-    if 'current_popularity' not in df.index:
-        df['current_popularity'] = 1000
+        # if ['current_popularity'] not in df.columns():
+        if 'current_popularity' not in df.index:
+            df['current_popularity'] = 1000
 
-    df = df.fillna(1000)
-    df['street'] = df['address'].str.split(', ', n=1, expand=True)[0]
-    df['city'] = df['address'].str.split(', ', n=1, expand=True)[1]
+        df = df.fillna(1000)
+        df['street'] = df['address'].str.split(', ', n=1, expand=True)[0]
+        df['city'] = df['address'].str.split(', ', n=1, expand=True)[1]
 
-    # extract important info
-    columns = ['name', 'id', 'street', 'city', 'current_popularity']
-    df = df[columns]
+        # extract important info
+        columns = ['name', 'id', 'street', 'city', 'current_popularity']
+        df = df[columns]
 
-    # convert to json
-    res_json =df.to_json(orient='records')
+        # convert to json
+        res_json =df.to_json(orient='records')
 
-    # check for consistency
-    df.to_csv(r'df2.csv', index=False)
+        # check for consistency
+        df.to_csv(r'df2.csv', index=False)
 
-    print(str(res_json))
-    sys.stdout.flush()
+        print(str(res_json))
+        sys.stdout.flush()
+
+    except:
+       print(r'[]')
+       sys.stdout.flush()
+       sys.exit(2)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
